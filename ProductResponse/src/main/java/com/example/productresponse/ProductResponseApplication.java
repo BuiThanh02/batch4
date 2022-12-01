2ea25b254dc8f496bbdd27c70880d7662f6711c7
buithanh281002 <buithanh281002@gmail.com>
2022-12-01 15:25:07 +0700

    product response

---
+++ ProductResponse/src/main/java/com/example/productresponse/ProductResponseApplication.java
@@ -0,0 +1,13 @@
+package com.example.productresponse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductResponseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductResponseApplication.class, args);
+    }
+
+}
---
+++ ProductResponse/src/test/java/com/example/productresponse/ProductResponseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.productresponse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductResponseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
