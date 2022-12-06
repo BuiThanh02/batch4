229e0bd861e5d513ea154919d105788b1c312d27
buithanh281002 <buithanh281002@gmail.com>
2022-12-06 20:48:08 +0700

    update

--- SpringProjectTasc/src/main/java/com/example/springprojecttasc/services/CategoryService.java
+++ SpringProjectTasc/src/main/java/com/example/springprojecttasc/services/CategoryService.java
@@ -31,13 +31,13 @@ public class CategoryService {
         // step  1 : validate request
         validateRequestCreateException(request);
 
-        if (!request.checkIsRoot()){
+        if (!request.checkIsRoot()) {
             // validate parent co ton tai khong
 
             Optional<Category> checkParentOpt = categoryRepository.findById(request.getParentId());
 
-            if (!checkParentOpt.isPresent()){
-                throw new ApiException(ERROR.INVALID_PARAM , "parent is invalid");
+            if (!checkParentOpt.isPresent()) {
+                throw new ApiException(ERROR.INVALID_PARAM, "parent is invalid");
             }
             Category parent = checkParentOpt.get();
             Category category = new Category();
@@ -45,8 +45,8 @@ public class CategoryService {
             category.setIcon(request.getIcon());
             category.setName(request.getName());
             category.setIsRoot(request.checkIsRoot() ? Constant.ONOFF.ON : Constant.ONOFF.OFF);
-            if(parent.getCategories().contains(category)){
-                throw new ApiException(ERROR.INVALID_PARAM , "AllReady exist!");
+            if (parent.getCategories().contains(category)) {
+                throw new ApiException(ERROR.INVALID_PARAM, "Already exist!");
             }
             Set<Category> childrenList = new HashSet<>();
             category.setCategories(childrenList);
@@ -70,91 +70,93 @@ public class CategoryService {
         return new BaseResponse(200, "success", category);
     }
 
-    private void validateRequestCreateException(CategoryRequest request) throws ApiException{
+    private void validateRequestCreateException(CategoryRequest request) throws ApiException {
 
-        if (StringUtils.isBlank(request.getIcon())){
-            throw new ApiException(ERROR.INVALID_PARAM , "icon is blank");
+        if (StringUtils.isBlank(request.getIcon())) {
+            throw new ApiException(ERROR.INVALID_PARAM, "icon is blank");
         }
 
-        if (StringUtils.isBlank(request.getName())){
-            throw new ApiException(ERROR.INVALID_PARAM , "name is banl");
+        if (StringUtils.isBlank(request.getName())) {
+            throw new ApiException(ERROR.INVALID_PARAM, "name is banl");
         }
 
-        if (StringUtils.isBlank(request.getDescription())){
-            throw new ApiException(ERROR.INVALID_PARAM , "description is Blank");
+        if (StringUtils.isBlank(request.getDescription())) {
+            throw new ApiException(ERROR.INVALID_PARAM, "description is Blank");
         }
 
-        if (request.checkIsRoot() && request.getParentId() != null){
-            throw new ApiException(ERROR.INVALID_PARAM , "level is invalid");
+        if (request.checkIsRoot() && request.getParentId() != null) {
+            throw new ApiException(ERROR.INVALID_PARAM, "level is invalid");
         }
 
-        if (!request.checkIsRoot() && request.getParentId() == null){
-            throw new ApiException(ERROR.INVALID_PARAM , "parent is blank");
+        if (!request.checkIsRoot() && request.getParentId() == null) {
+            throw new ApiException(ERROR.INVALID_PARAM, "parent is blank");
         }
     }
 
-    public BaseResponse updateCategory(Long id, CategoryRequest request) throws ApiException{
+    public BaseResponse updateCategory(Long id, CategoryRequest request) throws ApiException {
         validateRequestCreateException(request);
         Optional<Category> category = categoryRepository.findById(id);
-        if (!category.isPresent()){
-            throw new ApiException(ERROR.INVALID_PARAM , "category does not exist!");
+        if (!category.isPresent()) {
+            throw new ApiException(ERROR.INVALID_PARAM, "category does not exist!");
         }
         category.get().setIcon(request.getIcon());
         category.get().setDescription(request.getDescription());
         category.get().setName(request.getName());
         categoryRepository.save(category.get());
-        throw new ApiException(ERROR.SUCCESS , "success");
+        throw new ApiException(ERROR.SUCCESS, "success");
     }
 
-    public BaseResponse findByName(String name) throws ApiException{
+    public BaseResponse findByName(String name) throws ApiException {
         List<Category> categories = categoryRepository.findAllByNameContains(name);
-        if (categories.size() <= 0){
-            throw new ApiException(ERROR.SUCCESS , "no category founded");
+        if (categories.size() <= 0) {
+            throw new ApiException(ERROR.SUCCESS, "no category founded");
         }
         throw new ApiException(ERROR.SUCCESS, "success", categories);
     }
 
-    public BaseResponse findById(Long id) throws ApiException{
+    public BaseResponse findById(Long id) throws ApiException {
         Optional<Category> category = categoryRepository.findById(id);
-        if (!category.isPresent()){
-            throw new ApiException(ERROR.INVALID_PARAM , "category does not exist!");
+        if (!category.isPresent()) {
+            throw new ApiException(ERROR.INVALID_PARAM, "category does not exist!");
         }
         return new BaseResponse(200, "success", category.get());
     }
-    public BaseResponse findAllParent(Long id) throws ApiException{
+
+    public BaseResponse findAllParent(Long id) throws ApiException {
         Optional<Category> category = categoryRepository.findById(id);
-        if (!category.isPresent()){
-            throw new ApiException(ERROR.INVALID_PARAM , "category does not exist!");
+        if (!category.isPresent()) {
+            throw new ApiException(ERROR.INVALID_PARAM, "category does not exist!");
         }
         return new BaseResponse(200, "success", category.get().getParentCategories());
     }
-    public BaseResponse findAllChildren(Long id) throws ApiException{
+
+    public BaseResponse findAllChildren(Long id) throws ApiException {
         Optional<Category> category = categoryRepository.findById(id);
-        if (!category.isPresent()){
-            throw new ApiException(ERROR.INVALID_PARAM , "category does not exist!");
+        if (!category.isPresent()) {
+            throw new ApiException(ERROR.INVALID_PARAM, "category does not exist!");
         }
         return new BaseResponse(200, "success", category.get().getCategories());
     }
 
-    public BaseResponse delete(Long id) throws ApiException{
+    public BaseResponse delete(Long id) throws ApiException {
         Optional<Category> category = categoryRepository.findById(id);
-        if (!category.isPresent()){
-            throw new ApiException(ERROR.INVALID_PARAM , "category does not exist!");
+        if (!category.isPresent()) {
+            throw new ApiException(ERROR.INVALID_PARAM, "category does not exist!");
         }
 
-        if (category.get().getIsRoot() == 0){
-            for (Category cate:category.get().getParentCategories()
-                 ) {
+        if (category.get().getIsRoot() == 0) {
+            for (Category cate : category.get().getParentCategories()
+            ) {
                 cate.getCategories().remove(category.get());
             }
         }
 
         for (Category cate : category.get().getCategories()
-             ) {
+        ) {
             int numberOfParent = cate.getParentCategories().size();
-            if (numberOfParent == 1){
+            if (numberOfParent == 1) {
                 this.delete(cate.getId());
-            }else {
+            } else {
                 cate.getParentCategories().remove(category.get());
             }
         }
@@ -162,39 +164,40 @@ public class CategoryService {
         return new BaseResponse<>(200, "success");
     }
 
-    public SearchResponse findAll(SearchBody searchBody) throws ApiException{
-        String query = "SELECT * FROM category c WHERE 1 = 1";
-        if (searchBody.getName() != null && searchBody.getName().length() > 0){
-            query += "AND c.name LIKE '%" + searchBody.getName() + "%' ";
+    public SearchResponse findAll(SearchBody searchBody) throws ApiException {
+        StringBuilder query = new StringBuilder();
+        query.append("SELECT * FROM category c WHERE 1 = 1");
+        if (searchBody.getName() != null && searchBody.getName().length() > 0) {
+            query.append("AND c.name LIKE '%").append(searchBody.getName()).append("%' ");
         }
-        if (searchBody.getId() != null && searchBody.getId() > 0){
-            query += "AND c.id = " + searchBody.getId();
+        if (searchBody.getId() != null && searchBody.getId() > 0) {
+            query.append("AND c.id = ").append(searchBody.getId());
         }
-        query += " ORDER BY c.created_at " + searchBody.getSort();
+        query.append(" ORDER BY c.created_at ").append(searchBody.getSort());
         SearchResponse.Item item = new SearchResponse.Item();
         item.setCurrentPage(searchBody.getPage());
         item.setPageSize(searchBody.getLimit());
-        item.setItems(categoryRepository.findAllWithPage(query, searchBody.getPage() -1, searchBody.getLimit()));
+        item.setItems(categoryRepository.findAllWithPage(String.valueOf(query), searchBody.getPage() - 1, searchBody.getLimit()));
         item.setTotalItem(categoryRepository.count());
         return new SearchResponse(200, "success", item);
     }
 
-    public BaseResponse findAllChildrenByQuery(Long id) throws ApiException{
+    public BaseResponse findAllChildrenByQuery(Long id) throws ApiException {
         Optional<Category> optionalCategory = categoryRepository.findById(id);
-        if (!optionalCategory.isPresent()){
-            throw new ApiException(ERROR.INVALID_PARAM , "category does not exist!");
+        if (!optionalCategory.isPresent()) {
+            throw new ApiException(ERROR.INVALID_PARAM, "category does not exist!");
         }
-        String query = "select * from category c join category_relationship cr where c.id = cr.parent_id and c.id ="+id+" group by c.id";
-        return new BaseResponse(200 , "success", categoryRepository.findAll(query));
+        String query = "select * from category c join category_relationship cr where c.id = cr.parent_id and c.id =" + id + " group by c.id";
+        return new BaseResponse(200, "success", categoryRepository.findAll(query));
     }
 
-    public BaseResponse findAllParentByQuery(Long id) throws ApiException{
+    public BaseResponse findAllParentByQuery(Long id) throws ApiException {
         Optional<Category> optionalCategory = categoryRepository.findById(id);
-        if (!optionalCategory.isPresent()){
-            throw new ApiException(ERROR.INVALID_PARAM , "category does not exist!");
+        if (!optionalCategory.isPresent()) {
+            throw new ApiException(ERROR.INVALID_PARAM, "category does not exist!");
         }
-        String query = "select * from category c join category_relationship cr where c.id = cr.category_id and c.id ="+id+" group by c.id";
-        return new BaseResponse(200 , "success", categoryRepository.findAll(query));
+        String query = "select * from category c join category_relationship cr where c.id = cr.category_id and c.id =" + id + " group by c.id";
+        return new BaseResponse(200, "success", categoryRepository.findAll(query));
     }
 
 }
